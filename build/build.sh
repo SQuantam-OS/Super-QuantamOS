c05abaf498abf097d8b047325a8836f208934cda
Xaryx <163439609+GuestSneezeOSDev@users.noreply.github.com>
2024-08-01 21:04:18 +0400

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,44 +1,60 @@
 #!/bin/bash
-
+# QuantamOS build.sh fix
 sudo apt-get update
-sudo apt-get install -y gcc g++ make libncurses5-dev libssl-dev bc cdrkit syslinux
+sudo apt-get install -y build-essential libncurses-dev bison flex libssl-dev libelf-dev grub-pc-bin grub-efi-amd64-bin xorriso
+sudo apt-get install -y ghc cabal-install libx11-dev libxft-dev libxinerama-dev
+
 wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.10.2.tar.xz
-tar -xf linux-6.10.2.tar.xz
+tar -xvf linux-6.10.2.tar.xz
 cd linux-6.10.2
-# Fix #18
 make menuconfig
 make -j$(nproc)
 sudo make modules_install
 sudo make install
+
+if [ ! -f /boot/vmlinuz-6.10.2 ]; then
+    echo "Kernel image not found! Exiting."
+    exit 1
+fi
+
 cd ..
+mkdir -p quantam/rootfs/{bin,sbin,etc,proc,sys,usr/{bin,sbin}}
+
 wget https://busybox.net/downloads/busybox-1.33.1.tar.bz2
-tar -xjf busybox-1.33.1.tar.bz2
+tar -xvf busybox-1.33.1.tar.bz2
 cd busybox-1.33.1
 make defconfig
 make -j$(nproc)
-make install
-cd ..
-mkdir -p quantam/rootfs/{bin,sbin,etc,proc,sys,usr/{bin,sbin}}
-cp -a busybox-1.33.1/_install/* quantam/rootfs/
-echo "::sysinit:/etc/init.d/rcS" > quantam/rootfs/etc/inittab
-mkdir -p quantam/rootfs/etc/init.d
-echo -e "#!/bin/sh\nmount -t proc proc /proc\nmount -t sysfs sysfs /sys" > quantam/rootfs/etc/init.d/rcS
-chmod +x quantam/rootfs/etc/init.d/rcS
-mkdir -p quantam/xorg
-cd quantam/xorg
-git clone https://gitlab.freedesktop.org/xorg/xserver.git
-cd xserver
-./autogen.sh
-make -j$(nproc)
-sudo make install
-cd ../../..
-mkdir -p quantam/rootfs/etc/X11/xinit
-echo "exec /usr/bin/Xorg" > quantam/rootfs/etc/X11/xinit/xinitrc
-cd quantam/rootfs
-find . | cpio -o --format=newc | gzip > ../initramfs.gz
-cd ..
-mkdir -p quantam/isolinux
-cp /usr/lib/syslinux/isolinux.bin quantam/isolinux/
-echo -e "DEFAULT linux\nLABEL linux\n    KERNEL /boot/vmlinuz\n    APPEND root=/dev/ram0 initrd=/boot/initramfs.gz" > quantam/isolinux/isolinux.cfg
-mkisofs -o quantamos.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -R -J quantam/rootfs
-echo "QuantamOS ISO creation complete: quantamos.iso"
+make install CONFIG_PREFIX=../quantam/rootfs
+
+echo "::sysinit:/etc/init.d/rcS" > ../quantam/rootfs/etc/inittab
+mkdir -p ../quantam/rootfs/etc/init.d
+echo "#!/bin/sh" > ../quantam/rootfs/etc/init.d/rcS
+echo "mount -t proc none /proc" >> ../quantam/rootfs/etc/init.d/rcS
+echo "mount -t sysfs none /sys" >> ../quantam/rootfs/etc/init.d/rcS
+echo "/sbin/mdev -s" >> ../quantam/rootfs/etc/init.d/rcS
+chmod +x ../quantam/rootfs/etc/init.d/rcS
+
+cabal update
+cabal install xmonad
+cabal install xmonad-contrib
+
+cp ~/.cabal/bin/xmonad ../quantam/rootfs/usr/bin/
+
+mkdir -p ../quantam/rootfs/usr/share/X11/xorg.conf.d
+
+mkdir -p iso/boot/grub
+cp /boot/vmlinuz-6.10.2 iso/boot/vmlinuz
+cp /boot/initramfs.gz iso/boot/initramfs.gz
+
+cat > iso/boot/grub/grub.cfg << EOF
+set default=0
+set timeout=5
+
+menuentry "QuantamOS" {
+    linux /boot/vmlinuz root=/dev/sr0
+    initrd /boot/initramfs.gz
+}
+EOF
+
+grub-mkrescue -o quantam.iso iso
