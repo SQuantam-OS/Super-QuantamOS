69b4d90fe505150561f4f24b2c726a8878a9c96b
Xaryx <163439609+GuestSneezeOSDev@users.noreply.github.com>
2024-08-08 17:51:32 +0400

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,62 +5,46 @@ if [ "$EUID" -ne 0 ]; then
     echo "Please run this script as root or using sudo! or else"
     exit 1
 fi
-# Ok back to the script
-sudo apt-get update
-sudo apt-get install -y build-essential libncurses-dev bison flex libssl-dev libelf-dev grub-pc-bin grub-efi-amd64-bin xorriso
-sudo apt-get install -y ghc cabal-install libx11-dev libxft-dev libxinerama-dev
 
-wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.10.2.tar.xz
-tar -xvf linux-6.10.2.tar.xz
-cd linux-6.10.2
-make menuconfig
-make -j$(nproc)
-sudo make modules_install
-sudo make install
+supported_managers=("apt-get" "pacman" "yum" "zypper")
 
-if [ ! -f /boot/vmlinuz-6.10.2 ]; then
-    echo "Kernel image not found! Exiting."
-    exit 1
+read -p "Enter your package manager so we can continue: " user_manager
+
+if [[ " ${supported_managers[@]} " =~ " ${user_manager} " ]]; then
+    echo "Package manager '${user_manager}' is supported."
+    echo "Installing base-devel, git, and make..."
+    
+    case "${user_manager}" in
+        apt-get) sudo apt-get update; sudo apt-get install base-devel git make ;;
+        pacman) sudo pacman -Syu --needed base-devel git ;;
+        yum) sudo yum install base-devel git make ;;
+        zypper) sudo zypper install base-devel git make ;;
+    esac
+else
+    echo "Unsupported package manager. Choose from: ${supported_managers[*]}"
 fi
 
-cd ..
-mkdir -p quantam/rootfs/{bin,sbin,etc,proc,sys,usr/{bin,sbin}}
+start_time=$(date +%s)
 
-wget https://busybox.net/downloads/busybox-1.33.1.tar.bz2
-tar -xvf busybox-1.33.1.tar.bz2
-cd busybox-1.33.1
-make defconfig
-make -j$(nproc)
-make install CONFIG_PREFIX=../quantam/rootfs
-
-echo "::sysinit:/etc/init.d/rcS" > ../quantam/rootfs/etc/inittab
-mkdir -p ../quantam/rootfs/etc/init.d
-echo "#!/bin/sh" > ../quantam/rootfs/etc/init.d/rcS
-echo "mount -t proc none /proc" >> ../quantam/rootfs/etc/init.d/rcS
-echo "mount -t sysfs none /sys" >> ../quantam/rootfs/etc/init.d/rcS
-echo "/sbin/mdev -s" >> ../quantam/rootfs/etc/init.d/rcS
-chmod +x ../quantam/rootfs/etc/init.d/rcS
-
-# Use xmonad
-sudo apt-get install -y xmonad
-cp /usr/bin/xmonad ../quantam/rootfs/usr/bin/
-# Uninstall xmonad
-sudo apt-get remove xmonad
-mkdir -p ../quantam/rootfs/usr/share/X11/xorg.conf.d
-# Resolve #7
 cd ~/
-mkdir -p ~/quantam/rootfs/usr/local/bin
-curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
-cd /usr/local/bin
-sudo mv distrobox distrobox-enter distrobox-generate-entry distrobox-list distrobox-upgrade distrobox-assemble distrobox-ephemeral  distrobox-host-exec distrobox-rm distrobox-create  distrobox-export  distrobox-init distrobox-stop ~/quantam/rootfs/usr/local/bin
-# Initial patch
-cd ~/quantam
+git clone https://github.com/SQuantam-OS/qRoot
+cd ~/qRoot
+
+make menuconfig
+make -j$(nproc)
+cd output/images
+mkdir -p ~/quantam
+cp -R bzImage rootfs.ext4 ~/quantam/
+cd ~/quantam/
+mv rootfs.ext4 rootfs.tar
+tar xf rootfs.tar
+rm *.tar
 cd rootfs
 find . | cpio -o --format=newc | gzip > ../initramfs.gz
-# I Think that should fix it
 mkdir -p ~/iso/boot/grub
-cp /boot/vmlinuz-6.10.2 iso/boot/vmlinuz
-cp /boot/initramfs.gz iso/boot/initramfs.gz
+cd ~/quantam
+cp bzImage ~/iso/boot/vmlinuz
+cp initramfs.gz ~/iso/boot/initramfs.gz
 
 cat > iso/boot/grub/grub.cfg << EOF
 set default=0
@@ -72,4 +56,8 @@ menuentry "QuantamOS" {
 }
 EOF
 
+cd ~/
 grub-mkrescue -o quantam.iso iso
+end_time=$(date +%s)
+build_time=$((end_time - start_time))
+echo "Build finished. Time took to build: $build_time seconds/minutes.
